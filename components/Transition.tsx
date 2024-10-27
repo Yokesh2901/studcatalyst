cf5badd0382ea05632380b441235edc1fa8d2c2a
Yokesh S <142085506+Yokesh2901@users.noreply.github.com>
2024-10-27 22:19:03 +0530

    Update Transition.tsx

--- components/Transition.tsx
+++ components/Transition.tsx
@@ -1,6 +1,5 @@
 "use client"
-import {animate, motion} from 'framer-motion'
-import { exit } from 'process'
+import {motion} from 'framer-motion'
 import React from 'react'
 
 const TrasitionVariants = {
